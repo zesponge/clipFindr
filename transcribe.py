4f61224bf4c23354a3f1528b0d7145bb041be8df
zesponge <sumario82@gmail.com>
2024-02-27 20:57:02 -0500

    working transcription with time stamps

--- transcribe.py
+++ transcribe.py
@@ -1,35 +1,64 @@
 from google.cloud import videointelligence
+import os
 
-def transcribe_video(input_url):
-   """Transcribe a video from a Google Cloud Storage bucket."""
-   client = videointelligence.VideoIntelligenceServiceClient()
+def transcribe_video(input_uri):
+    # set key credentials file path
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\sumar\Desktop\opportune-ego-415620-d9343252e488.json"
 
-    # Define features to be extracted
-   features = [videointelligence.enums.Feature.TEXT_DETECTION]
+    """Transcribe speech from a video stored on GCS bucket."""
 
-    # Configure the request
-   config = videointelligence.types.TextDetectionConfig()
-   config.language_hints.append("en-US")
+    video_client = videointelligence.VideoIntelligenceServiceClient()
+    features = [videointelligence.Feature.SPEECH_TRANSCRIPTION]
 
-    # Construct the video context
-   context = videointelligence.types.VideoContext(text_detection_config=config)
+    config = videointelligence.SpeechTranscriptionConfig(
+        language_code="en-US", enable_automatic_punctuation=True
+    )
+    video_context = videointelligence.VideoContext(speech_transcription_config=config)
 
-    # Perform the request
-   operation = client.annotate_video(input_uri=input_url, features=features, video_context=context)
+    operation = video_client.annotate_video(
+        request={
+            "features": features,
+            "input_uri": input_uri,
+            "video_context": video_context,
+        }
+    )
 
-    # Wait for the operation to complete
-   result = operation.result(timeout=900)
+    print("\nProcessing video for speech transcription.")
 
-    # Extract the transcription results
-   transcription = []
-   for annotation_result in result.annotation_results:
-    for text_annotation in annotation_result.text_annotations:
-       transcription.append(text_annotation.text)
+    result = operation.result(timeout=600)
 
-    return transcription
+    # There is only one annotation_result since only
+    # one video is processed.
+    annotation_results = result.annotation_results[0]
+    for speech_transcription in annotation_results.speech_transcriptions:
+
+        # The number of alternatives for each transcription is limited by
+        # SpeechTranscriptionConfig.max_alternatives.
+        # Each alternative is a different possible transcription
+        # and has its own confidence score.
+        for alternative in speech_transcription.alternatives:
+            print("Alternative level information:")
+
+            print("Transcript: {}".format(alternative.transcript))
+            print("Confidence: {}\n".format(alternative.confidence))
+
+            
+            print("Word level information:")
+            for word_info in alternative.words:
+                word = word_info.word
+                start_time = word_info.start_time
+                end_time = word_info.end_time
+                print(
+                    "\t{}s - {}s: {}".format(
+                        start_time.seconds + start_time.microseconds * 1e-6,
+                        end_time.seconds + end_time.microseconds * 1e-6,
+                        word,
+                    )
+                )
 
 if __name__ == "__main__":
-    input_url = "gs://scrobblesearch/doordashAd.mp4"
-    video_transcription = transcribe_video(input_url)
-    for text in video_transcription:
-        print(text)
+    # Replace this variable with the URI of your video in Google Cloud Storage
+    input_uri = "gs://scrobblesearch/doordashAd.mp4"
+
+    # Transcribe the video and store the result in a variable
+    video_transcription = transcribe_video(input_uri)
