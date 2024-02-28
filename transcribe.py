9317613ee4cdaf4b6a9cdcc5281a23c93835cd1f
zesponge <sumario82@gmail.com>
2024-02-27 22:45:05 -0500

    working video label annotation

--- transcribe.py
+++ transcribe.py
@@ -48,7 +48,7 @@ def transcribe_video(input_uri):
             print("group information:")
             for word_info in alternative.words:
                 word = word_info.word
-                start_time = 
+                start_time = word_info.start_time
                 end_time = word_info.end_time
                 print(
                     "\t{}s - {}s: {}".format(
@@ -57,10 +57,104 @@ def transcribe_video(input_uri):
                         word,
                     )
                 )
+    return alternative.transcript
+
+
+def annotate_video(input_uri):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\sumar\Desktop\opportune-ego-415620-d9343252e488.json"
+
+    video_client = videointelligence.VideoIntelligenceServiceClient()
+    features = [videointelligence.Feature.LABEL_DETECTION]
+
+    mode = videointelligence.LabelDetectionMode.SHOT_AND_FRAME_MODE
+    config = videointelligence.LabelDetectionConfig(label_detection_mode=mode)
+    context = videointelligence.VideoContext(label_detection_config=config)
+
+    operation = video_client.annotate_video(
+        request={
+            "features": features,
+            "input_uri": input_uri,
+            "video_context": context,
+        }
+    )
+
+    print("\nProcessing video for label annotations:")
+
+    result = operation.result(timeout=180)
+    print("\nlabel annotations Finished processing.\n")
+
+    segment_labels = result.annotation_results[0].segment_label_annotations
+    for i, segment_label in enumerate(segment_labels):
+        print("Video label description: {}".format(segment_label.entity.description))
+        for category_entity in segment_label.category_entities:
+            print(
+                "\tLabel category description: {}".format(category_entity.description)
+            )
+
+        for i, segment in enumerate(segment_label.segments):
+            start_time = (
+                segment.segment.start_time_offset.seconds
+                + segment.segment.start_time_offset.microseconds / 1e6
+            )
+            end_time = (
+                segment.segment.end_time_offset.seconds
+                + segment.segment.end_time_offset.microseconds / 1e6
+            )
+            positions = "{}s to {}s".format(start_time, end_time)
+            confidence = segment.confidence
+            print("\tSegment {}: {}".format(i, positions))
+            print("\tConfidence: {}".format(confidence))
+        print("\n")
+
+    # Process shot level label annotations
+    shot_labels = result.annotation_results[0].shot_label_annotations
+    for i, shot_label in enumerate(shot_labels):
+        print("Shot label description: {}".format(shot_label.entity.description))
+        for category_entity in shot_label.category_entities:
+            print(
+                "\tLabel category description: {}".format(category_entity.description)
+            )
+
+        for i, shot in enumerate(shot_label.segments):
+            start_time = (
+                shot.segment.start_time_offset.seconds
+                + shot.segment.start_time_offset.microseconds / 1e6
+            )
+            end_time = (
+                shot.segment.end_time_offset.seconds
+                + shot.segment.end_time_offset.microseconds / 1e6
+            )
+            positions = "{}s to {}s".format(start_time, end_time)
+            confidence = shot.confidence
+            print("\tSegment {}: {}".format(i, positions))
+            print("\tConfidence: {}".format(confidence))
+        print("\n")
+
+    # Process frame level label annotations
+    frame_labels = result.annotation_results[0].frame_label_annotations
+    for i, frame_label in enumerate(frame_labels):
+        print("Frame label description: {}".format(frame_label.entity.description))
+        for category_entity in frame_label.category_entities:
+            print(
+                "\tLabel category description: {}".format(category_entity.description)
+            )
+
+        # Each frame_label_annotation has many frames,
+        # here we print information only about the first frame.
+        frame = frame_label.frames[0]
+        time_offset = frame.time_offset.seconds + frame.time_offset.microseconds / 1e6
+        print("\tFirst frame time offset: {}s".format(time_offset))
+        print("\tFirst frame confidence: {}".format(frame.confidence))
+        print("\n")
+    
+
+
 
 if __name__ == "__main__":
     # Replace this variable with the URI of your video in Google Cloud Storage
     input_uri = "gs://scrobblesearch/subAd.mp4"
 
     # Transcribe the video and store the result in a variable
-    video_transcription = transcribe_video(input_uri)
+    # video_transcription = transcribe_video(input_uri)
+    annotate_video(input_uri)
+
