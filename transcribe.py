02d264c10cdabde3cd49fb38c88ac34722d09039
zesponge <sumario82@gmail.com>
2024-02-27 23:06:03 -0500

    cut down printing

--- transcribe.py
+++ transcribe.py
@@ -83,29 +83,6 @@ def annotate_video(input_uri):
     result = operation.result(timeout=180)
     print("\nlabel annotations Finished processing.\n")
 
-    segment_labels = result.annotation_results[0].segment_label_annotations
-    for i, segment_label in enumerate(segment_labels):
-        print("Video label description: {}".format(segment_label.entity.description))
-        for category_entity in segment_label.category_entities:
-            print(
-                "\tLabel category description: {}".format(category_entity.description)
-            )
-
-        for i, segment in enumerate(segment_label.segments):
-            start_time = (
-                segment.segment.start_time_offset.seconds
-                + segment.segment.start_time_offset.microseconds / 1e6
-            )
-            end_time = (
-                segment.segment.end_time_offset.seconds
-                + segment.segment.end_time_offset.microseconds / 1e6
-            )
-            positions = "{}s to {}s".format(start_time, end_time)
-            confidence = segment.confidence
-            print("\tSegment {}: {}".format(i, positions))
-            # print("\tConfidence: {}".format(confidence))
-        print("\n")
-
     # Process shot level label annotations
     shot_labels = result.annotation_results[0].shot_label_annotations
     for i, shot_label in enumerate(shot_labels):
@@ -127,24 +104,7 @@ def annotate_video(input_uri):
             positions = "{}s to {}s".format(start_time, end_time)
             confidence = shot.confidence
             print("\tSegment {}: {}".format(i, positions))
-            # print("\tConfidence: {}".format(confidence))
-        print("\n")
-
-    # Process frame level label annotations
-    frame_labels = result.annotation_results[0].frame_label_annotations
-    for i, frame_label in enumerate(frame_labels):
-        print("Frame label description: {}".format(frame_label.entity.description))
-        for category_entity in frame_label.category_entities:
-            print(
-                "\tLabel category description: {}".format(category_entity.description)
-            )
-
-        # Each frame_label_annotation has many frames,
-        # here we print information only about the first frame.
-        frame = frame_label.frames[0]
-        time_offset = frame.time_offset.seconds + frame.time_offset.microseconds / 1e6
-        print("\tFirst frame time offset: {}s".format(time_offset))
-        # print("\tFirst frame confidence: {}".format(frame.confidence))
+            print("\tConfidence: {}".format(confidence))
         print("\n")
     
 
